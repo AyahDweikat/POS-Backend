bc28e58010a49f015e65a8d1584559e007a5ee0a
AyahDweikat <dweikatayah99@gmail.com>
2023-06-20 14:43:38 +0300

    -- cors

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 import express from 'express';
+import bodyParser from "body-parser";
+import cors from "cors";
+
 
 import * as dotenv from 'dotenv'
 dotenv.config()
-
 import connectDB from './DB/connection.js';
 import initApp from './src/app.router.js';
-
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-
+app.use(cors())
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE, PATCH")
+  next();
+});
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 
 initApp(app, express)
